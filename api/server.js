9c3fd78e5bbd875606e21a9706e3fe839ffee8b4
Erick Gonzalez <ericksinbox@gmail.com>
2021-06-26 00:48:41 -0400

    updated server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const app = express()
-app.use(express.json())
-app.use(helmet())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'connect-src': [
+            'erickgonzalez.herokuapp.com',          
+            "'self'",
+        ],
+        'script-src': [
+            "'self'",
+        ],        
+        'img-src': [
+            "'self'"
+        ],
+         'font-src': [
+            "'self'",
+            'google',
+        ],
+        'frame-src': ['*.google.com', '*.vimeo.com'], 
+        'default-src': ['erickgonzalez.herokuapp.com', "'self'"],
+        'script-src-elem': ['erickgonzalez.herokuapp.com', "'self'"],
+        'style-src': ["'self'", 'erickgonzalez.herokuapp.com']
+      },
+    },
+  })
+);
 
 module.exports = app;
