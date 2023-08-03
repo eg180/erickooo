a22cc56e383454cfcaeb8cbbcce3d7344b585373
Erick Gonzalez <ericksinbox@gmail.com>
2023-08-03 12:46:07 +0200

    add extra exclamation point

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-require('dotenv').config()
-const path = require('path');
+require("dotenv").config();
+const path = require("path");
 // var __dirname = path.resolve();
-const express = require('express');
-const app = require('./api/server.js');
+const express = require("express");
+const app = require("./api/server.js");
 app.use(express.json());
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
 
+	app.get("/", (_, res) => {
+		// if you want to serve a SPA using Express you totally can!
+		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+		//   res.json({ api: "up"})
+	});
 
-
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-
-    app.get('/', (_, res) => {
-
-        // if you want to serve a SPA using Express you totally can!
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    //   res.json({ api: "up"})
-})
-    
-    // app.use("/api/*", (__, res) => {
-    //   res.json({ api: "up"})
-    // })s
+	// app.use("/api/*", (__, res) => {
+	//   res.json({ api: "up"})
+	// })s
 }
 
 const port = process.env.PORT || 5000;
 // server.set("port", port);
 
-
 app.use("/api", (__, res) => {
-    res.json({ api: "it lives!" })
+	res.json({ api: "it lives!!" });
 });
 
 app.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
